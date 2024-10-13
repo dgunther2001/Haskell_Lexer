3339665320ef9654b2cf59d2f83174df1f56109b
Daniel Gunther <132302961+dgunther2001@users.noreply.github.com>
2024-10-12 23:02:13 -0400

    Made naming more clear

--- src/Main.hs
+++ src/Main.hs
@@ -12,31 +12,31 @@ data Token
 
 lexer :: String -> Either String [Token]
 lexer [] = Right []
-lexer (char:char_list)
-  | isSpace char = 
-    lexer char_list
-  | isDigit char = 
-    let (digits, rest) = span isDigit (char:char_list) 
+lexer (current_character:rest_of_input)
+  | isSpace current_character = 
+    lexer rest_of_input
+  | isDigit current_character = 
+    let (digits, rest) = span isDigit (current_character:rest_of_input) 
     in case lexer rest of 
       Right tokens -> Right (TokenNumber (read digits) : tokens)
       Left err -> Left err
-  | char == '\'' =
-    lexCharacterLiteral char_list
-  | char == '"' =
-    lexStringLiteral char_list
-  | isAlpha char || char == '_' =
-    let (ident, rest) = span (\next_char -> isAlphaNum next_char || next_char =='_') (char:char_list)
+  | current_character == '\'' =
+    lexCharacterLiteral rest_of_input
+  | current_character == '"' =
+    lexStringLiteral rest_of_input
+  | isAlpha current_character || current_character == '_' =
+    let (ident, rest) = span (\next_char -> isAlphaNum next_char || next_char =='_') (current_character:rest_of_input)
     in case lexer rest of 
       Right tokens -> Right (TokenIdentifier ident : tokens)
       Left err -> Left err
-  | char == '+' =
-    fmap (TokenPlus char :) (lexer char_list)
-  | char == '=' =
-    fmap (TokenAssign char :) (lexer char_list)
-  | char == ';' = 
-    fmap (TokenSemiColon char :) (lexer char_list)
+  | current_character == '+' =
+    fmap (TokenPlus current_character :) (lexer rest_of_input)
+  | current_character == '=' =
+    fmap (TokenAssign current_character :) (lexer rest_of_input)
+  | current_character == ';' = 
+    fmap (TokenSemiColon current_character :) (lexer rest_of_input)
   | otherwise = 
-    Left ("Unexpected character (" ++ [char] ++ ")")
+    Left ("Unexpected character (" ++ [current_character] ++ ")")
 
 
 lexCharacterLiteral :: String -> Either String [Token]
@@ -55,7 +55,7 @@ lexStringLiteral input_stream =
     ('"':rest_of_stream_string_terminus_removed) ->  -- if we find it, everything following the closing '"' is stored in the remaining input
       case lexer rest_of_stream_string_terminus_removed of -- recursively call the lexer on the remaining input, and pattern match the current case when we pass back up the stack
         Right tokens -> Right (TokenString strContent : tokens) -- if the recursive call successfully generated a list of tokens, append the token string to the front of the list
-        Left err -> Left err -- if the recursive lexer call returned a string (an error), propograte the error back up the call stack
+        Left err -> Left err -- if the recursive lexer call returned a string (an error), propogate the error back up the call stack
     _ -> Left "Unterminated string literal"  -- if the rest of the input does not contain a closing '"' we return an error propogated back up as an optional string
 
 
